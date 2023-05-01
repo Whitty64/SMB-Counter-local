2629b72b7f135a35d046a477934482d711077428
Taylor Whitfield <taywhitfield1995@gmail.com>
2023-05-01 16:40:29 -0400

    Add begin / to src to fix build error

--- src/components/TotalSMBCounter.vue
+++ src/components/TotalSMBCounter.vue
@@ -4,7 +4,7 @@
       Small Business Counter
     </h1>
     <div class="flex justify-center items-center">
-      <img src="src/assets/small-business-icon.png" alt="" class="w-32 pr-8" />
+      <img src="/src/assets/small-business-icon.png" alt="" class="w-32 pr-8" />
       <div class="montserrat text-center text-6xl font-bold text-cargo-blue">
         {{ Math.floor(TotalSMBCounter).toLocaleString("en-US") }}
       </div>
